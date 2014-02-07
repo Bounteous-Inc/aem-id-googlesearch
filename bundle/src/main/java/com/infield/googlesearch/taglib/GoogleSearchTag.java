0c17456cabeaab42eec3d43b8e475e7599a615c0
Gaetan Marmasse <gaetan@infielddesign.com>
2014-02-06 21:26:42 -0800

    Created Taglib and TLD file

---
+++ bundle/src/main/java/com/infield/googlesearch/taglib/GoogleSearchTag.java
@@ -0,0 +1,55 @@
+
+/*************************************************************************
+*
+* Copyright 2014 Infield Design
+* All Rights Reserved.
+* __________________
+*
+*  Author Gaetan Marmasse <gaetan@infielddesign.com>
+**************************************************************************/
+package com.infield.googlesearch.taglib;
+
+import javax.servlet.jsp.tagext.SimpleTagSupport;
+
+import java.io.IOException;
+import java.util.LinkedList;
+
+import javax.servlet.jsp.JspException;
+
+import org.osgi.service.component.ComponentContext;
+
+import com.infield.googlesearch.GoogleSearchService;
+import com.infield.googlesearch.model.ResultItem;
+import com.infield.googlesearch.model.ResultList;
+
+
+public class GoogleSearchTag extends SimpleTagSupport {
+
+	private String q;
+	private String currentTab;
+	
+	
+	@Override
+	public void doTag() throws JspException, IOException {
+		if (this.q.length() > 0 && this.currentTab.length() > 0){
+			GoogleSearchService googleService = new GoogleSearchService();
+			ResultList resultList = googleService.getResults(q, currentTab);
+			LinkedList<ResultItem> resultItems = resultList.getResultItems();
+			getJspContext().setAttribute("resultList",resultList);
+			getJspContext().setAttribute("resultItems",resultItems);
+		}
+	}
+	
+	public String getQ(){
+		return q;
+	}
+	public void setQ(String q){
+		this.q = q;
+	}
+	public String getCurrentTab(){
+		return currentTab;
+	}
+	public void setCurrentTab(String currentTab){
+		this.currentTab = currentTab;
+	}
+}
